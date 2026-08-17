9b661000a7d8fde9e880e89ae0a020f100031a79
Alex Molnar <alex.molnar@ing.com>
2026-08-17 18:32:01 +0200

    Exposed more model details

--- api/app/client/local.py
+++ api/app/client/local.py
@@ -2,6 +2,15 @@ from requests import post, get
 from json import loads, dumps
 from os import getenv
 
+model_descriptions = {
+    "gemma4:e4b": "Flagship model with 4b context window. Ideal for heavy tasks.",
+    "mistral:7b": "Large model for bigger workloads. Expect more accurate anserw but a bigger waiting time",
+    "qwen3.5:4b": "Middle class model, up to date processing, with middle of the pack memory overhead.",
+    "phi3:mini": "Mini model. Ideal for quick small tasks",
+    "llama3.2:latest": "Ollama's own edge model. Should be ideal for small to medium tasks",
+    "qwen2.5:3b": "Small edge model for quick and accurate responses"
+}
+
 class Client:
     def __init__(self):
         self.url = getenv('OLLAMA_API_URL', 'http://localhost:11434/api/{path}')
@@ -22,7 +31,28 @@ class Client:
     def get_models(self):
         response = get(self.url.format(path='tags'))
         if response.status_code == 200:
-            return [m['name'] for m in response.json()['models']]
+            print(dumps(response.json()['models'], indent=2))
+            for m in response.json()['models']:
+                print(m)
+                print('capabilities' in m)
+            return {
+                "models": [
+                    {
+                        'name': m['name'], 
+                        'displayName': m['name'].split(':')[0],
+                        'thinking': 'thinking' in m.get('capabilities', []),
+                        'tools_support': 'tools' in m.get('capabilities', []),
+                        'allowed': True
+                    } for m in response.json()['models']
+                ] + [{
+                    'name': 'gpt-5.1:latest',
+                    'displayName': 'GPT 5.1 Cloud',
+                    'thinking': True,
+                    'tools_support': True,
+                    'allowed': False
+                }],
+                "default": "qwen2.5:3b"
+            }
 
     def get_version(self):
         response = get(self.url.format(path='version'))
